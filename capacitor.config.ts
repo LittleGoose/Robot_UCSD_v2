901df177e07ea003d551c01b66884470761f15d1
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-08-31 11:53:10 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.test2',
+  appName: 'test2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
