e31509e57d4e241997b3600d1cb004cb9649cd98
melany890 <84870600+melany890@users.noreply.github.com>
2023-09-07 11:00:06 -0700

    adelanto botones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+/*import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -40,4 +40,4 @@ describe('AppComponent', () => {
       '/folder/outbox'
     );
   });
-});
+}); */
