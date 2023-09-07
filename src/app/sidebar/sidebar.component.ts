b11bd0c1d8d35e1d14fd76bf696a5ce1ca38a948
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-09-07 13:57:21 -0700

    Merge advances in Sprint1

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { ScrollDetail } from '@ionic/angular';
+
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent  implements OnInit {
+
+  rootPage1 = 'Panel1Page';
+  rootPage2 = 'Panel2Page';
+  rootPage3 = 'Panel3Page';
+
+  facial_expresions:string[] = ["Happy","Sad","Mad","Angry","Crazy"];
+  body_gestures:string[] = ["Nod","Turn","Walk","Side_head","Bow"];
+  tone_of_voice:string[] = ["Excited","Timid","Sad","Slow","Fast"];
+  speach:string[] = ["Listen","Talk","Hum","Scream","Agree"];
+  routines:string[] = ["Dance_1","Conversation_1","Coffe_talk_2","Apologyze","Aggreable_2"];
+
+  options:string[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.generateItems();
+  }
+
+  private generateItems() {
+    
+    let totalexpressions = this.facial_expresions.length;
+    for (let i = 0; i < totalexpressions; i++) {
+      this.options.push(` ${this.facial_expresions[i]}`);
+    }
+
+    let totalgestures = this.body_gestures.length;
+    for (let i = 0; i < totalgestures; i++) {
+      this.options.push(` ${this.body_gestures[i]}`);
+    }
+
+    let totalvoices = this.tone_of_voice.length;
+    for (let i = 0; i < totalvoices; i++) {
+      this.options.push(` ${this.tone_of_voice[i]}`);
+    }
+
+    let totalspeach = this.speach.length;
+    for (let i = 0; i < totalspeach; i++) {
+      this.options.push(` ${this.speach[i]}`);
+    }
+
+    let totalroutines = this.routines.length;
+    for (let i = 0; i < totalroutines; i++) {
+      this.options.push(` ${this.routines[i]}`);
+    }
+    
+  }
+
+  onIonInfinite(ev: any) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+
+  funcionalidad(){}
+
+  // Aqui inica las funciones para hacer el Scroll
+  handleScrollStart() {
+    console.log('scroll start');
+  }
+
+  handleScroll(ev: CustomEvent<ScrollDetail>) {
+    console.log('scroll', ev.detail);
+  }
+
+  handleScrollEnd() {
+    console.log('scroll end');
+  }
+
+}
