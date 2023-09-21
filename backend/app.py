cab063f6a5691095dacd363b5b41fbe6f3d8b822
Liz2310 <67923198+Liz2310@users.noreply.github.com>
2023-09-21 14:30:01 -0700

    Updates

--- backend/app.py
+++ backend/app.py
@@ -1,127 +1,33 @@
-from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask import Flask, jsonify
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 import datetime
+from datetime import datetime
 import bson
 import yaml
-import json
+import os
 
 app = Flask(__name__)
 
-client = MongoClient('0.0.0.0', 27017) # Connect to mongo client (local level)
+client = MongoClient("0.0.0.0", 27017) # Connect to mongo client (local level)
 db = client["ROBOT_UCSD"] # Access/creation of data base
 
 facial_expressions = db["facial_expressions"] # Creation/Access of table Expressions
-
-# Dummy entry for Expressions table
-# dummy_expression_post = {
-#     "id_in_robot": "DummyId",
-#     "expression_name": "Happy",
-#     "description": "Expression for happiness",
-#     "animation": """import { AnimatedSprite, Texture } from 'pixi.js';
-#                     const alienImages = [
-#                         'image_sequence_01.png',
-#                         'image_sequence_02.png',
-#                         'image_sequence_03.png',
-#                         'image_sequence_04.png',
-#                     ];
-#                     const textureArray = [];
-#                     for (let i = 0; i < 4; i++){
-#                         const texture = Texture.from(alienImages[i]);
-#                         textureArray.push(texture);
-#                     }
-#                     const animatedSprite = new AnimatedSprite(textureArray);
-#                     """,
-#         "status": "Active"
-# }
-# facial_expressions.insert_one(dummy_expression_post)
-
 body_gestures = db["body_gestures"] # Creation/Access of table Movements
-
-# Dummy entry for Movements table
-# dummy_movements_post = {
-#     "id_in_robot": "DummyId",
-#     "movement_name": "Nod",
-#     "description": "Nod to the user",
-#     "status": "Active"
-# }
-# body_gestures.insert_one(dummy_movements_post)
-
 tones_of_voice = db["tones_of_voice"] # Creation/Access of table Tones of Voice
-
-# Dummy entry for Tones of Voice table
-# dummy_tones_post = {
-#     "id_in_robot": "DummyId",
-#     "tone_name": "Energetic",
-#     "description": "Eskeler mode",
-#     "status": "Unactive"
-# }
-# tones_of_voice.insert_one(dummy_tones_post)
-
 speech_elements = db["speech_elements"] # Creation/Access of table Speech
-
-# Dummy entry for Speech table
-# dummy_speech_post = {
-#     "id_in_robot": "DummyId",
-#     "element_name": "Talk",
-#     "description": "Casual conversation",
-#     "utterance" : "Hello there",
-#     "status": "Active"
-# }
-# speech_elements.insert_one(dummy_speech_post)
-
 routines = db["routines"] #Creation/Access of table Routines
 
-# Dummy entry for Routines table
-# routine = {
-#     "Segment_1": {
-#         "Block_1": {
-#             "Type": "facial_expression",
-#             "Level": 2,
-#             "Label": "Happy"
-#             }
-#     },
-#     "Segment_2": {
-#         "Block_1": {
-#             "Type": "speech",
-#             "Level": 3,
-#             "Label": "Hum"
-#         }
-#     }
-#     }
-
-# dummy_routine_post = {
-#     "user": "User1",
-#     "last_modified": datetime.datetime.now(tz=datetime.timezone.utc),
-#     "name": "Dance_1",
-#     "file": yaml.dump(routine)}
-
-# json.dumps() json.loads() https://stackoverflow.com/questions/26745519/converting-dictionary-to-json
-# routines.insert_one(dummy_routine_post)
-
-
-
 # Main app
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def root():
-    return "Hello from Flask"
+    return "MAIN FLASK ROUTE"
 
-
-# @app.route("/create_yaml", methods=['GET'])
-# def create_yaml():
-#     data = {
-#         "Block_1": {"Talk" : 1 , "Walk" : 3, "Energetic" : 2, "Happy": 3},
-#         "Block_2": {"Nod" : 1 , "Hum" : 3},
-#         "Block_3": {"Talk" : 2 , "Walk" : 2, "Energetic" : 2, "Happy": 2},
-#         "Block_4": {"Listen" : 3}
-#     }
-
-#     yaml = yaml.dump(data)
-
-@app.route("/fetch_from_db", methods=['GET'])
-def create_yaml():
+# Fetch entries from all tables to send to sidebar angular component
+# Return entries in a json format
+@app.route("/fetch_tables_from_db", methods=["GET"])
+def fetch_from_db():
         
-    data = {}
+    data = {} 
 
     facial_expressions_entries = []
     for entry in facial_expressions.find():
@@ -155,27 +61,42 @@ def create_yaml():
 
     return jsonify(data)
 
-# @app.route("/fetch_subroutines", methods=['GET'])
-# def create_yaml():
-#     return "Here I implement returning subroutine (YAML) file as a JSON object"
+# CREATE
+# Receive a the routine object to enconde in binary
+# Add to entry and upload to database
+@app.route("/save_yaml", methods=["POST"])
+def save_yaml(routine):
+    # TODO convert routine from angular data type to python dict
+    try:
+        routine_post = {
+            "user": "User1",
+            "last_modified": datetime.datetime.now(tz=datetime.timezone.utc),
+            "name": "Dance_1",
+            "file":  bson.encode(routine)}
+        routines.insert_one(routine_post)
+        print("Insert completed")
+    except Exception as e:
+        print("An error ocurred: ", e)
+
+# READ
+# Download routine from angular app
+@app.route("/download_routine", methods=["GET"])
+def download_routine(routine):
+    # TODO convert routine from angular data type to python dict
+    try:
+        home = os.path.expanduser("~")
+        x = datetime.now()
+        file_name = home + x.strftime('%d-%m-%Y-%H-%M-%S.yaml')
+        with open(file_name, 'w') as fp:
+            fp.write(yaml.dump(routine))
+        print("Download completed")
+    except Exception as e:
+        print("An error ocurred: ", e)
+
+# TODO Retrieve most recent routine
+# TODO UPDATE
+# TODO DELETE
+
 
 if __name__== "__main__":
     app.run(debug=True)
-
-# R2
-# @app.route('/', methods=['GET', 'POST'])
-# def root():
-#     if request.method=='POST':
-#         content = request.form['content']
-#         degree = request.form['degree']
-#         todos.insert_one({'content': content, 'degree': degree})
-#         return redirect(url_for('root'))
-
-#     all_todos = todos.find()
-#     return render_template('index.html', todos=all_todos)
-
-
-# @app.post('/<id>/delete/')
-# def delete(id):
-#     todos.delete_one({"_id": ObjectId(id)})
-#     return redirect(url_for('root'))
