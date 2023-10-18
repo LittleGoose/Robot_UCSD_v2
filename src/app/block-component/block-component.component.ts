f8b6a9e9a94861528fc25665534945fc55ec7e71
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-10-17 19:03:09 -0700

    Change divide

--- src/app/block-component/block-component.component.ts
+++ src/app/block-component/block-component.component.ts
@@ -279,7 +279,7 @@ export class BlockComponentComponent implements AfterViewInit {
 
     // Iterate through the coordinates and add unique x values to the Set
 
-    const divide = 4;
+    const divide = 7;
 
     this.reset_edges();
 
